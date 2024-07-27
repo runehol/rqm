e25890b244efcc1b34941fb7677d383b2d087308
Rune Holm <rune.holm@gmail.com>
2024-07-27 22:38:39 +0200

    add add benchmarks

--- benchmark/benchmark_rqm.cpp
+++ benchmark/benchmark_rqm.cpp
@@ -1,20 +1,74 @@
+#include "rqm/rqm.h"
 #include <benchmark/benchmark.h>
 
-
-static void BM_add(benchmark::State& state) {
+static void int64_t_add(benchmark::State &state)
+{
     // Perform setup here
-    int a = 2;
-    int b = 1;
+    int64_t a = 99999999;
+    int64_t b = -999999999;
 
-    size_t n_iters = 0;
-    for (auto _ : state) {
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
         // This code gets timed
-        int c = a + b;
+        int64_t c = a + b;
         benchmark::DoNotOptimize(c);
-        ++n_iters;
-
     }
-    state.SetItemsProcessed(n_iters);
 }
 
-BENCHMARK(BM_add);
+BENCHMARK(int64_t_add);
+
+static void RQM_add_same_sign(benchmark::State &state)
+{
+    // Perform setup here
+    rqm::num a = 0x123456789;
+    rqm::num b = 0x123456789;
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        rqm::num c = a + b;
+        benchmark::DoNotOptimize(c);
+    }
+}
+
+BENCHMARK(RQM_add_same_sign);
+
+static void RQM_add_diff_sign_a_larger(benchmark::State &state)
+{
+    // Perform setup here
+    rqm::num a = 0x123456789;
+    rqm::num b = -0x12345678;
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        rqm::num c = a + b;
+        benchmark::DoNotOptimize(c);
+    }
+}
+
+BENCHMARK(RQM_add_diff_sign_a_larger);
+
+static void RQM_add_diff_sign_b_larger(benchmark::State &state)
+{
+    // Perform setup here
+    rqm::num a = -0x12345678;
+    rqm::num b = 0x123456789;
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        rqm::num c = a + b;
+        benchmark::DoNotOptimize(c);
+    }
+}
+
+BENCHMARK(RQM_add_diff_sign_b_larger);
