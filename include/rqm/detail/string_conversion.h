813ca5b633dbba725e0263ebb05cf417e2646255
Rune Holm <rune.holm@gmail.com>
2024-07-28 14:03:58 +0200

    fast path the single-digit case

--- include/rqm/detail/string_conversion.h
+++ include/rqm/detail/string_conversion.h
@@ -19,15 +19,25 @@ namespace rqm
             return pos;
         }
 
-        [[nodiscard]] static inline uint32_t to_string_buffer_estimate(uint32_t n_digits)
+        [[nodiscard]] static inline constexpr uint32_t to_string_buffer_estimate(uint32_t n_digits)
         {
-            // 1 for possible sign, maximum number of decimals per digits times number of digits
-            return 1 + n_decimals_in_digit_high * n_digits;
+            // 1 for possible sign, maximum number of decimals per digits times number of digits, and one for the terminating zero in the fast path
+            return 1 + 1 + n_decimals_in_digit_high * n_digits;
         }
 
-        [[nodiscard]] static inline std::string_view to_string(char *dest_end, const numview n)
+        [[nodiscard]] static inline std::string_view to_string(char *end, const numview n)
         {
             if(n.signum == 0) return std::string_view("0"); // special-case zeros
+
+            // fast path single-digit value
+            if(n.n_digits == 1)
+            {
+                signed_double_digit_t v = signed_double_digit_t(n.digits[0]) * n.signum;
+                uint32_t max_possible = to_string_buffer_estimate(1);
+                char *pos = end - max_possible;
+                int n_written = snprintf(pos, max_possible, "%lld", v);
+                return std::string_view(pos, n_written);
+            }
 
             MAKE_STACK_TEMPORARY_NUMVIEW(value, n.n_digits);
             MAKE_STACK_TEMPORARY_NUMVIEW(value2, n.n_digits);
@@ -36,8 +46,7 @@ namespace rqm
             value.signum = 1; // it's positive now. negativeness is handled at the end
             value.n_digits = n.n_digits;
 
-            char *pos = dest_end;
-            char *end = pos;
+            char *pos = end;
 
             while(value.n_digits > 0)
             {
