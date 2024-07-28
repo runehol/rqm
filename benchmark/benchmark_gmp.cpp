1116087bd3c27fde79e6912458249275d46bbcfe
Rune Holm <rune.holm@gmail.com>
2024-07-28 21:06:55 +0200

    added GMP benchmarks

---
+++ benchmark/benchmark_gmp.cpp
@@ -0,0 +1,134 @@
+#include <benchmark/benchmark.h>
+#include <stdio.h>
+
+#include <gmp.h>
+
+static void GMP_add_same_sign(benchmark::State &state)
+{
+    mpz_t a, b, c;
+    mpz_inits(a, b, c, nullptr);
+
+    // Perform setup here
+    mpz_set_str(a, "0x123456789", 0);
+    mpz_set_str(b, "0x123456789", 0);
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        mpz_add(c, a, b);
+        benchmark::DoNotOptimize(c);
+    }
+    mpz_clears(a, b, c, nullptr);
+}
+
+BENCHMARK(GMP_add_same_sign);
+
+static void GMP_add_diff_sign_a_larger(benchmark::State &state)
+{
+    mpz_t a, b, c;
+    mpz_inits(a, b, c, nullptr);
+
+    // Perform setup here
+    mpz_set_str(a, "0x123456789", 0);
+    mpz_set_str(b, "-0x12345678", 0);
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        mpz_add(c, a, b);
+        benchmark::DoNotOptimize(c);
+    }
+    mpz_clears(a, b, c, nullptr);
+}
+
+BENCHMARK(GMP_add_diff_sign_a_larger);
+
+static void GMP_add_diff_sign_b_larger(benchmark::State &state)
+{
+    mpz_t a, b, c;
+    mpz_inits(a, b, c, nullptr);
+
+    // Perform setup here
+    mpz_set_str(a, "-0x12345678", 0);
+    mpz_set_str(b, "0x123456789", 0);
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        mpz_add(c, a, b);
+        benchmark::DoNotOptimize(c);
+    }
+    mpz_clears(a, b, c, nullptr);
+}
+
+BENCHMARK(GMP_add_diff_sign_b_larger);
+
+static void GMP_mul(benchmark::State &state)
+{
+    mpz_t a, b, c;
+    mpz_inits(a, b, c, nullptr);
+
+    // Perform setup here
+    mpz_set_str(a, "0x123456789", 0);
+    mpz_set_str(b, "0x12345678", 0);
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        mpz_mul(c, a, b);
+        benchmark::DoNotOptimize(c);
+    }
+    mpz_clears(a, b, c, nullptr);
+}
+
+BENCHMARK(GMP_mul);
+
+static void GMP_mul_num_with_digit(benchmark::State &state)
+{
+    mpz_t a, c;
+    mpz_inits(a, c, nullptr);
+    // Perform setup here
+    mpz_set_str(a, "0x123456789", 0);
+    int32_t b = 0x12345678;
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        mpz_mul_si(c, a, b);
+        benchmark::DoNotOptimize(c);
+    }
+    mpz_clears(a, c, nullptr);
+}
+
+BENCHMARK(GMP_mul_num_with_digit);
+
+static void GMP_div_num_with_digit(benchmark::State &state)
+{
+    mpz_t a, c;
+    mpz_inits(a, c, nullptr);
+    // Perform setup here
+    mpz_set_str(a, "0x123456789", 0);
+    int32_t b = 0x12345678;
+
+    benchmark::DoNotOptimize(a);
+    benchmark::DoNotOptimize(b);
+    for(auto _: state)
+    {
+        // This code gets timed
+        mpz_tdiv_q_ui(c, a, b);
+        benchmark::DoNotOptimize(c);
+    }
+    mpz_clears(a, c, nullptr);
+}
+
+BENCHMARK(GMP_div_num_with_digit);
